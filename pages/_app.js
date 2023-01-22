5b0daea5a2ddcfa7cbe119ddf161111efb05d8b7
Mace Akkari <mace.akkari@relyfe.com>
2023-01-22 16:02:30 +0000

    add capture details

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,28 @@ import Loading from "../components/Loading";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
+import { doc, setDoc, Timestamp } from "firebase/firestore";
+import { useEffect } from "react";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      const document = doc(db, `users/${user.uid}`);
+      setDoc(
+        document,
+        {
+          email: user.email,
+          lastSeen: Timestamp.now(),
+          photoURL: user.photoURL,
+        },
+        { merge: true }
+      );
+    }
+  }, [user]);
 
   if (loading) return <Loading />;
   if (!user) return <Login />;
