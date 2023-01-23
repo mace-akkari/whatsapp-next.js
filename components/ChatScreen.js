899c9cd681b6da4446fb85707beaa22df4d89893
Mace Akkari <mace.akkari@relyfe.com>
2023-01-23 10:10:02 +0000

    add the [id] Component

---
+++ components/ChatScreen.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function ChatScreen() {
+  return (
+    <Container>
+      <h1>here</h1>
+    </Container>
+  );
+}
+
+export default ChatScreen;
+
+const Container = styled.div``;
