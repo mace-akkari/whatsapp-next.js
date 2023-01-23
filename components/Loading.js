2922d0a2ffaee4a347f482d999af6876507fe0a4
Mace Akkari <mace.akkari@relyfe.com>
2023-01-23 12:59:20 +0000

    update logo source

--- components/Loading.js
+++ components/Loading.js
@@ -2,7 +2,7 @@ import { Circle } from "better-react-spinkit";
 
 function Loading() {
   const LOGO =
-    "http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png ";
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/2042px-WhatsApp.svg.png";
   return (
     <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
       <div>
--- pages/login.js
+++ pages/login.js
@@ -16,7 +16,7 @@ function Login() {
       </Head>
 
       <LoginContainer>
-        <Logo src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png " />
+        <Logo src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/2042px-WhatsApp.svg.png" />
         <Button onClick={signIn} variant="outlined">
           Sign in with Google
         </Button>
